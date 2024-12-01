156f7bc2007733ddf8c2f345f1aa69b76bc4b9ba
Cezary Babik <babik.czarek@gmail.com>
2024-12-01 16:46:31 +0100

    calendar_view

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { DateTime } from 'luxon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.css',
-  standalone: true
+  styleUrls: ['./calendar.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class CalendarComponent {
+  today: DateTime = DateTime.local();
+  firstDayOfMonth: DateTime = this.today.startOf('month');
+  weekDays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  daysOfMonth: DateTime[] = [];
 
+  constructor() {
+    this.generateDaysOfMonth();
+  }
+
+  // Funkcja do generowania dni w miesiącu
+  generateDaysOfMonth() {
+    const start = this.firstDayOfMonth.startOf('week'); // Pierwszy dzień widoczny w kalendarzu
+    const end = this.firstDayOfMonth.endOf('month').endOf('week'); // Ostatni dzień widoczny w kalendarzu
+    let current = start;
+
+    while (current <= end) {
+      this.daysOfMonth.push(current);
+      current = current.plus({ days: 1 }); // Przechodzimy do kolejnego dnia
+    }
+  }
 }
